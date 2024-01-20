caa85bd27f86ae1940a500a46dc7e88f01595852
Gouranegithub <gouraneabdellah@gmail.com>
2024-01-20 13:44:45 +0100

    creat a class called student and dispalay it

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
@@ -14,7 +20,10 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello world";
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L,
+						"Gourane",
+						21, "gouraneInpt@gmail.com", LocalDate.of(2003, 02, 13)));
 	}
 }
